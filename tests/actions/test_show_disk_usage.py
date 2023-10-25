b656bb0283f5c5f5cc6a8a09c80af05221de47d4
vcscunha <vcs.cunha@icloud.com>
2023-10-25 19:41:22 -0300

    req 05, parte 01: implementa a funcao test_espaco_usado_diretorio_vazio()

--- tests/actions/test_show_disk_usage.py
+++ tests/actions/test_show_disk_usage.py
@@ -1,1 +1,11 @@
 from pro_filer.actions.main_actions import show_disk_usage  # NOQA
+
+
+# 01: verificar a saida do diretorio vazio
+def test_espaco_usado_diretorio_vazio(capsys):
+    context = {"all_files": []}
+    show_disk_usage(context)
+    captured = capsys.readouterr()
+    espaco_usado = captured.out
+
+    assert "Total size: 0" in espaco_usado
