dcde3d4d71075b731761950265e1fe57e0009a80
vcscunha <vcs.cunha@icloud.com>
2023-10-25 22:28:38 -0300

    req 05, parte 01: refatora funcao test_espcao_usado_diretorio_vazio()

--- tests/actions/test_show_disk_usage.py
+++ tests/actions/test_show_disk_usage.py
@@ -1,61 +1,18 @@
 from pro_filer.actions.main_actions import show_disk_usage  # NOQA
-from unittest.mock import patch
+import pytest
 
 
 # 01: verificar a saida do diretorio vazio
-def test_espaco_usado_diretorio_vazio(capsys):
-    context = {"all_files": []}
+@pytest.mark.parametrize(
+        "context, expected",
+        [
+            (
+                {"all_files": []},
+                "Total size: 0",
+            ),
+        ],
+)
+def test_espaco_usado_diretorio_vazio(capsys, context, expected):
     show_disk_usage(context)
     captured = capsys.readouterr()
-    espaco_usado = captured.out
-
-    assert "Total size: 0" in espaco_usado
-
-
-# 02: verificar o espaco usado e tamanho dos arquivos
-def test_espaco_usado(capsys, context, tmp_path):
-    arquivo_01 = tmp_path / "arquivo_01.txt"
-    arquivo_01.write_text("Conteudo do arquivo 01")
-
-    arquivo_02 = tmp_path / "arquivo_2.txt"
-    arquivo_02.write_text("Conteudo do arquivo 02")
-
-    context = {
-        "all_files": [
-            str(arquivo_01),
-            str(arquivo_02),
-        ]
-    }
-
-    with patch(
-        "pro_filer.cli_helpers._get_printable_file_path"
-    ) as funcao_substituta:
-        funcao_substituta.return_value = "mock_caminho_arquivo"
-
-        show_disk_usage(context)
-        captured = capsys.readouterr()
-        saida_espaco_usado = captured.out
-
-        # arquivos estao corretos
-        assert "'mock_caminho_arquivo'" in saida_espaco_usado
-        assert "Conteudo do arquivo 01" in saida_espaco_usado
-        assert "Conteudo do arquivo 02" in saida_espaco_usado
-
-        # tamanho total esta correto
-        assert "Total size: 44" in saida_espaco_usado
-
-        # ordenacao de saida
-        linhas = saida_espaco_usado.split("\n")
-
-        linhas_arquivo = []
-        for linha in linhas:
-            if linha.startswith("'mock_caminho_arquivo'"):
-                linhas_arquivo.append(linha)
-
-        def obter_tamanho_arquivo(linha):
-            return int(linha.split()[-2])
-
-        assert linhas_arquivo == sorted(
-            linhas_arquivo,
-            key=obter_tamanho_arquivo,
-        )
+    assert captured.out == expected
