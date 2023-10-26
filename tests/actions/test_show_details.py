05b63ffec791eaff8f06a2c24ab7847791bf0b11
vcscunha <vcs.cunha@icloud.com>
2023-10-26 13:38:40 -0300

    req 04: importa date from datetime e corrige as saidas que utilizam datas, na variavel str_5 e na funcao test_exibe_data_formato_correto()

--- tests/actions/test_show_details.py
+++ tests/actions/test_show_details.py
@@ -1,12 +1,16 @@
 from pro_filer.actions.main_actions import show_details  # NOQA
 
 import pytest
+from datetime import date
+
+data_atual = date.today()
+data_formatada = data_atual.strftime("%Y-%m-%d")
 
 str_1 = "File name: arquivo_teste.txt\n"
 str_2 = "File size in bytes: 0\n"
 str_3 = "File type: file\n"
 str_4 = "File extension: .txt\n"
-str_5 = "Last modified date: 2023-10-25\n"
+str_5 = f"Last modified date: {data_formatada}\n"
 
 str_6 = "File extension: [no extension]\n"
 
@@ -30,23 +34,17 @@ def test_exibe_mensagem_correta(capsys, context, expected):
 
 
 # 02: usa o formato de data correto
-@pytest.mark.parametrize(
-    "context, expected",
-    [
-        (
-            {"base_path": "arquivo_teste.txt"},
-            f"{str_1}{str_2}{str_3}{str_4}{str_5}",
-        ),
-    ],
-)
-def test_exibe_data_formato_correto(capsys, context, expected):
+def test_exibe_data_formato_correto(capsys):
+    context = {"base_path": "arquivo_teste.txt"}
+
     with open("arquivo_teste.txt", "w"):
         pass
     show_details(context)
     captured = capsys.readouterr()
 
     linha_info_data = captured.out.splitlines()[-1].strip()
-    assert linha_info_data == "Last modified date: 2023-10-25"
+
+    assert linha_info_data == f"Last modified date: {data_formatada}"
 
 
 # 03: arquivo não existente
